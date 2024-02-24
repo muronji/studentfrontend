f3f25739d29df40df15ae2b738e588878e2674aa
muronji <muronjimulievi@gmail.com>
2024-02-24 22:54:43 +0300

    added post functions

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,10 +1,27 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Paper } from "@mui/material";
+import { Paper, Button } from "@mui/material";
+import { useState } from "react";
 
 export default function Student() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name: name, address: address };
+    console.log(student);
+    fetch("http://localhost:8080/student/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(student),
+    }).then(() => {
+      console.log("New Student added");
+    });
+  };
   return (
     <Box
       component="form"
@@ -15,13 +32,17 @@ export default function Student() {
       autoComplete="off"
     >
       <Paper elevation={3} style={paperStyle}>
-        <h1 style={{color:"blue"}}><u>Add Student</u></h1>
+        <h1 style={{ color: "blue" }}>
+          <u>Add Student</u>
+        </h1>
         <TextField
           id="outlined-basic"
           label="Student Name"
           variant="outlined"
           style={{ margin: "10px" }}
           fullWidth
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <TextField
           id="outlined-basic"
@@ -29,7 +50,13 @@ export default function Student() {
           variant="outlined"
           style={{ margin: "10px" }}
           fullWidth
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
         />
+
+        <Button variant="contained" onClick={handleClick}>
+          Submit
+        </Button>
       </Paper>
     </Box>
   );
