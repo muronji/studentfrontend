fd172b0ebffc8e9aca1e62f8b5a3dd252bb5406c
muronji <muronjimulievi@gmail.com>
2024-03-05 08:44:38 +0300

    added fetching functionality

--- src/components/Student.js
+++ src/components/Student.js
@@ -2,12 +2,13 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Paper, Button } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Student() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
+  const [student, setStudent] = useState([]);
   const handleClick = (e) => {
     e.preventDefault();
     const student = { name: name, address: address };
@@ -22,6 +23,14 @@ export default function Student() {
       console.log("New Student added");
     });
   };
+
+  useEffect(() => {
+    fetch("http://localhost:8080/student/getAll")
+      .then((res) => res.json())
+      .then((result) => {
+        setStudent(result);
+      });
+  });
   return (
     <Box
       component="form"
@@ -57,6 +66,21 @@ export default function Student() {
         <Button variant="contained" onClick={handleClick}>
           Submit
         </Button>
+      </Paper>
+      <Paper elevation={3} style={paperStyle}>
+        {students.map((student) => (
+          <Paper
+            elevation={6}
+            style={{ margin: "10px", padding: "15px", textAlign: "left" }}
+            key={student.id}
+          >
+            Id:{student.id}
+            <br />
+            Name:{student.name}
+            <br />
+            Address:{student.address}
+          </Paper>
+        ))}
       </Paper>
     </Box>
   );
